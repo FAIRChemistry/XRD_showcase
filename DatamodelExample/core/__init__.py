15e161b6ffbd55bf523e96492fd4e22e46bd9fd6
sH4MbLe5 <r.schoemig25@gmail.com>
2022-09-16 11:05:00 +0200

    delete redundant files

--- DatamodelExample/core/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dataset import Dataset
-from .specinf import SpecInf
-
-__doc__ = "This is just a tiny example to show how a notebook for XRD can look like when provided with some data."
-
-__all__ = [
-    "Dataset",
-    "SpecInf",
-]
